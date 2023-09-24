b1b40f9d90e5b295058ed3038f382f317e574bcc
f3rn4nd0000 <42982873+f3rn4nd0000@users.noreply.github.com>
2023-09-24 11:22:17 -0300

    testa sites

--- src/main.py
+++ src/main.py
@@ -1,4 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+from bs4.element import Comment
 
+URL_2 = "https://www.planetf1.com/"
+URL_1 = "https://www.motorsport.com/"
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+def text_from_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)  
+    return u" ".join(t.strip() for t in visible_texts)
+
+if __name__ == "__main__":
+
+    response1 = requests.get(URL_1).text
+    response2 = requests.get(URL_2).text
+
+    print(text_from_html(response1))
+    # text_from_html(response2)
+
+# bs1 = BeautifulSoup(response1.text)
+# bs2 = BeautifulSoup(response2.text, 'html.parser')
+# texts = bs2.find_all(text=True)
+# visible_texts = filter(tag_visible, texts)
+# print(texts)
+# print(u" ".join(t.strip() for t in visible_texts))
+# print(bs2.find("body"))
+
+
+
+
+"""
 import tweepy
 from decouple import config
 
@@ -19,6 +58,7 @@ print(user_info)
 # for tweet in public_tweets:
 #     print(tweet.text)
 
+"""
 
 """
 
