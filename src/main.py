0c1bb048db3a218aa0502fc845783c1bae77f6c1
f3rn4nd0000 <42982873+f3rn4nd0000@users.noreply.github.com>
2023-10-13 22:57:06 -0300

    feat: Adiciona funcao para coletar todas as equipes

--- src/main.py
+++ src/main.py
@@ -38,6 +38,12 @@ api = tweepy.Client(
 URL_3 = "https://www.formula1.com/"
 URL_4 = "https://www.formula1points.com/"
 
+DRIVERS_URL = "https://www.formula1.com/en/drivers"
+ALEXANDER_ALBON = "https://www.formula1.com/en/drivers/alexander-albon.html"
+IMG_CLASS = "image fom-image fom-adaptiveimage-fallback"
+DRIVERS_STANDING_2023 = "https://www.formula1.com/en/results.html/2023/drivers"
+
+
 ### FUNCOES AUXILIARES
 def tag_visible(element):
     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
@@ -66,6 +72,17 @@ def insert_pilot(db:database.Database, pilot_data) -> None:
     pilot_id = pilots_collection.insert_one(pilot).inserted_id
     print(pilot_id)
 
+"""
+def insert_team(db:database.Database, team_data) -> None:
+    teams_collection = db.teams
+    team = {
+        "Name":,
+        "Pilot1":,
+        "Pilot2":,
+    }
+    team_id = teams_collection.insert_one(team).inserted_id
+"""
+
 def search_pilot(db:database.Database, pilot_name) -> None:
     pilots_collection = db.pilots
     query = pilots_collection.find_one({"Name": str(pilot_name)})
@@ -82,11 +99,6 @@ def text_from_html(html):
     texts = soup.findAll(text=True)
     visible_texts = filter(tag_visible, texts)  
     return u" ".join(t.strip() for t in visible_texts)
-
-DRIVERS_URL = "https://www.formula1.com/en/drivers"
-ALEXANDER_ALBON = "https://www.formula1.com/en/drivers/alexander-albon.html"
-IMG_CLASS = "image fom-image fom-adaptiveimage-fallback"
-DRIVERS_STANDING_2023 = "https://www.formula1.com/en/results.html/2023/drivers"
 
 # Retorna Um elemento BeautifulSoup, função auxiliar parr
 def return_soup(html) -> BeautifulSoup:
@@ -113,6 +125,33 @@ def get_all_pilots(html):
         all_pilots.append((pilot_name, pilot_codename, pilot_callsign))
     return all_pilots
 
+TEAMS_URL = "https://www.formula1.com/en/teams.html"
+def get_all_teams(html) -> None:
+    soup = return_soup(html)
+    list_of_teams = soup.find("div", attrs={"class": "container listing team-listing"}).find_all("a", attrs={"class":"listing-link"})
+    
+    all_teams = []
+    for team in list_of_teams:
+        team_obj = json.loads(team["data-tracking"])
+        team_name = team_obj.get("path")
+        # print(team_obj)
+        driver1 = team.find("div", attrs={"class": "driver"})
+        driver1_name = driver1.get_text().strip().rstrip().replace('\n',' ')
+        driver2 = driver1.findNext("div", attrs={"class": "driver"})
+        driver2_name = driver2.get_text().strip().rstrip().replace('\n',' ')
+
+        team = {
+            "Name": team_name,
+            "Driver1": driver2_name,
+            "Driver2": driver1_name
+        }
+        all_teams.append(team)
+    
+    for team in all_teams:
+        print(team)
+
+        # all_teams.append(team)
+        # print(team_name)
 
 # BAIXA IMAGEM DO PILOTO DE ACORDO COM SEU NOME
 def get_pilot_img(html, pilot_name):
@@ -167,6 +206,7 @@ def generate_excel(labels, rows, pilot_name='all_pilots'):
 def generate_graph():
     ...
 
+"""
 def main():
     list_of_pilots = get_all_pilots(DRIVERS_URL+".html")
 
@@ -179,12 +219,14 @@ def main():
     # INSERE DADOS DO PILOTO NO BANCO
     db = connect_db()
         
-        # insert_pilot(db, pilot)
+    # insert_pilot(db, pilot)
 
     # PROCURA POR PILOTO/ EXEMPLO MAX VERSTAPPEN
     search_pilot(db, "Max Verstappen")
-
-
+"""
+def main():
+    print(TEAMS_URL)
+    get_all_teams(TEAMS_URL)
 
 # APENAS COMO TESTE INSERE OS RESULTADOS EM UM EXCEL DE UM PILOTO
 if __name__ == "__main__":
